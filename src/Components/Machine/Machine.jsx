d3297999e671c2d99a6ae6cd09b93f52a7e39cf9
ssemlitz <sdsemlitz@yahoo.com>
2023-02-13 13:11:52 -0500

    refactor machine styling using module

--- src/Components/Machine/Machine.jsx
+++ src/Components/Machine/Machine.jsx
@@ -1,12 +1,14 @@
 // import {React, useState, useEffect} from 'react'
+import styles from './Machine.module.css'
+
 
 const Machine = ({ s1, s2, s3 }) => {
   
   const winner = (s1 === s2) && (s2 === s3);
   
   return (
-    <div className='Machine'>
-      <p style={{fontSize: '50px', backgroundColor: 'purple'}}>{s1} {s2} {s3}</p>
+    <div className={styles.Machine}>
+      <p>{s1} {s2} {s3}</p>
       <p className={winner?'Machine-win':'Machine-lose'}>{winner ? 'Winner!' : 'Loser!'}</p>
     </div>
   )  
