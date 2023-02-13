166460f7d95d44f9b6c498250db08246451c2be1
ssemlitz <sdsemlitz@yahoo.com>
2023-02-13 12:47:33 -0500

    add basic styling the Machine component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-// import Machine from './Components/Machine/Machine';
+import Machine from './Components/Machine/Machine';
 import Friend from './Components/Friend/Friend';
 
 function App({props}) {
   return (
     <div>
+      
+      <Machine 
+        s1="X"
+        s2="X"
+        s3="X"
+      />
       <Friend 
         name='Elton'
         hobbies={['singing','dancing','running']}
+      />  
+      <Friend 
+        name='Jennie'
+        hobbies={['gaming','volleyball','exercising']}
       />  
     </div>
   );
--- src/Components/Machine/Machine.jsx
+++ src/Components/Machine/Machine.jsx
@@ -1,29 +1,13 @@
-import {React, useState, useEffect} from 'react'
+// import {React, useState, useEffect} from 'react'
 
 const Machine = ({ s1, s2, s3 }) => {
-  
-  const [row, setRow] = useState([s1,s2,s3])
-  
-  function onClick(event) {
-    const fruits = ['🍒', '🍊', '🍋']
-    s1 = fruits[Math.floor(Math.random() * fruits.length)]
-    s2 = fruits[Math.floor(Math.random() * fruits.length)]
-    s3 = fruits[Math.floor(Math.random() * fruits.length)]
-    return (
-      setRow([s1,s2,s3])  
-    )
-  }
-  useEffect(()=>{
-    
-  }, [setRow])
   
   const winner = (s1 === s2) && (s2 === s3);
   
   return (
-    <div>
-      <p>{s1} {s2} {s3}</p>
-      <p>{winner ? 'Winner!' : 'Loser!'}</p>
-      <button onClick={onClick}>Spin</button>
+    <div className='Machine'>
+      <p style={{fontSize: '50px', backgroundColor: 'purple'}}>{s1} {s2} {s3}</p>
+      <p className={winner?'Machine-win':'Machine-lose'}>{winner ? 'Winner!' : 'Loser!'}</p>
     </div>
   )  
 }
