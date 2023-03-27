9bad482cc2697bec17856eabaa9e089f1e2efee2
ssemlitz <sdsemlitz@yahoo.com>
2023-03-27 12:36:15 -0400

    refactor joke component to remove state

--- src/DadJokes/Joke.jsx
+++ src/DadJokes/Joke.jsx
@@ -1,46 +1,41 @@
 import './Joke.css'
-import { useState } from 'react';
 
 const Joke = ({joke, handleVote}) => {
-  
-  const [votes, setVotes] = useState(joke.votes)
 
   const handleClick = (amount) => {
     handleVote(joke.id, amount)
   }
 
   function getColor() {
-    if (votes >= 15) {
+    if (joke.votes >= 15) {
       return "#4CAF50";
-    } else if (votes >= 12) {
+    } else if (joke.votes >= 12) {
       return "#8BC34A";
-    } else if (votes >= 9) {
+    } else if (joke.votes >= 9) {
       return "#CDDC39";
-    } else if (votes >= 6) {
+    } else if (joke.votes >= 6) {
       return "#FFEB3B";
-    } else if (votes >= 3) {
+    } else if (joke.votes >= 3) {
       return "#FFC107";
-    } else if (votes >= 0) {
+    } else if (joke.votes >= 0) {
       return "#FF9800";
     } else {
       return "#f44336";
     }
   }
 
-
-
   function getEmoji() {
-    if (votes >= 15) {
+    if (joke.votes >= 15) {
       return "em em-rolling_on_the_floor_laughing";
-    } else if (votes >= 12) {
+    } else if (joke.votes >= 12) {
       return "em em-laughing";
-    } else if (votes >= 9) {
+    } else if (joke.votes >= 9) {
       return "em em-smiley";
-    } else if (votes >= 6) {
+    } else if (joke.votes >= 6) {
       return "em em-slightly_smiling_face";
-    } else if (votes >= 3) {
+    } else if (joke.votes >= 3) {
       return "em em-neutral_face";
-    } else if (votes >= 0) {
+    } else if (joke.votes >= 0) {
       return "em em-confused";
     } else {
       return "em em-angry";
@@ -52,7 +47,7 @@ const Joke = ({joke, handleVote}) => {
       <div className="voteBtns">
         <i className='voteBtn fa-sharp fa-solid fa-arrow-down' onClick={() => handleClick(-1)}></i>
         <div className='votes' style={{borderColor: getColor(), borderWidth: '3px'}}>
-        {votes}
+        {joke.votes}
         </div>
         <i className='voteBtn fa-sharp fa-solid fa-arrow-up' onClick={() => handleClick(1)}></i>
       </div>
